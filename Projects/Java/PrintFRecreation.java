1bc74dbfaa3802137203eaf6b3cb8caf8ccb0f24
AustinJamesZiliak <Ajziliak@stu.naperville203.org>
2023-11-02 18:21:41 -0500

    fixed a dumb bug and now have full input implemented

--- Projects/Java/PrintFRecreation.java
+++ Projects/Java/PrintFRecreation.java
@@ -26,13 +26,12 @@ public class PrintFRecreation
         int precision = 0;
         String conversionCharacter;
         int endOfFormat = 0;
+        int IndexAtEndOfWidth = 0;
         //initilize objects 
         Scanner key = new Scanner(System.in);
-        
         //take input from user
         System.out.println("Enter printf formated text\n-->");
         unFormated = key.nextLine();
-        
         //begin cutting up String "unFormated"
         Begining = unFormated.substring(0, unFormated.indexOf("%"));
         endOfFormat = unFormated.indexOf("%");
@@ -49,7 +48,6 @@ public class PrintFRecreation
             commaSeparation = true;
         else if (flag == ' ')
             spaceMinusSign = true;
-        
         for (int n = 2, widthIndex = endOfFormat+n; 
         unFormated.charAt(widthIndex) == '0' || 
         unFormated.charAt(widthIndex) == '1' || 
@@ -63,11 +61,25 @@ public class PrintFRecreation
         unFormated.charAt(widthIndex) == '9'
         ;n+=1, widthIndex = endOfFormat+n)
         {
-            System.out.println(endOfFormat+n);
-            System.out.println(widthIndex);
-            System.out.println(unFormated.charAt(widthIndex));
             width = (width*10)+Character.getNumericValue(unFormated.charAt(widthIndex));
-            key.nextLine();
+            IndexAtEndOfWidth = endOfFormat+n+1;
         }
+        IndexAtEndOfWidth += 1;
+        for (int n = 0, precisionIndex = IndexAtEndOfWidth+n; 
+        unFormated.charAt(precisionIndex) == '0' || 
+        unFormated.charAt(precisionIndex) == '1' || 
+        unFormated.charAt(precisionIndex) == '2' || 
+        unFormated.charAt(precisionIndex) == '3' || 
+        unFormated.charAt(precisionIndex) == '4' || 
+        unFormated.charAt(precisionIndex) == '5' || 
+        unFormated.charAt(precisionIndex) == '6' || 
+        unFormated.charAt(precisionIndex) == '7' || 
+        unFormated.charAt(precisionIndex) == '8' || 
+        unFormated.charAt(precisionIndex) == '9'
+        ;n+=1, precisionIndex = IndexAtEndOfWidth+n)
+        {
+            precision = (precision*10)+Character.getNumericValue(unFormated.charAt(precisionIndex));
+        }
+        System.out.println("% "+flag+" "+width+" . "+precision);
     }
 }
